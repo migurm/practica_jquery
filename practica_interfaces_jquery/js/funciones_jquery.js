94cf70c7d869630ce608dfe3eddead9eccc9c1b4
Miguel Angel Rueda Moreno <migue.rueda.90@gmail.com>
2024-01-29 12:25:17 +0100

    Swipper OK, funcionalidad volver arriba OK

--- practica_interfaces_jquery/js/funciones_jquery.js
+++ practica_interfaces_jquery/js/funciones_jquery.js
@@ -27,6 +27,27 @@ function cambio_vista(){
 }
 
 
+function volver_arriba(){
+    $('body').append('<button id="volver-arriba">Volver arriba</button>');
+
+    // Cuando el usuario hace scroll de 20px desde la parte superior del documento, muestra el botón
+    $('.content__page').scroll(function() {
+        console.log("Estamos scroleando amigos");
+        if ($(this).scrollTop() > 20) {
+            console.log("Debería aparecer el botoncito");
+            $('#volver-arriba').fadeIn();
+        } else {
+            $('#volver-arriba').fadeOut();
+        }
+    });
+
+    // Cuando el usuario hace clic en el botón, desplaza el documento hasta la parte superior
+    $('#volver-arriba').click(function() {
+        $('.content__page').animate({scrollTop: 0}, 800);
+        return false;
+    });
+}
+
 
 
 
@@ -34,17 +55,17 @@ function cambio_vista(){
 //Cuando todo esté listo:
 
 $(document).ready(function(){
+    volver_arriba();
     var modo_oscuro_activo = sessionStorage.getItem('modo_oscuro') === 'true'; //Vemos el modo en el que andamos
     if(!modo_oscuro_activo) cambio_vista(); //Aplicamos el estilo
     $('#cambio_vista').on('click', cambio_vista); //Asignamos el evento al botón de cambio de vistaç
     console.log("Cargando jquery propio...");
     console.log("Hola desde jquery!");
     //Actuaciones jQuery generales.
-    domotica();
+    //domotica(); Fue demasiado ambicioso.
     //Función para la descarga del CV... POR HACER
     $('.user-info__btn').on('click', function(){
         console.log("Todavía no se programó la descarga del CV");
-
     })
     
 
@@ -55,6 +76,8 @@ $(document).ready(function(){
         var ejeY = event.pageY;
         //console.log(`Evento de raton en el tiempo: ${timestamp} y estoy en la posicio Y: ${ejeY}`);
     })
+
+
 
 
 
@@ -80,7 +103,20 @@ $(document).ready(function(){
                       },
                 });
 
-
+                console.log("Agregando pop ups");
+                // Agregamos los pop ups para las imágenes de los artículos.
+                $('.article__image').on('click', function(){
+                    var imagen_articulo = $(this).attr('src'); // Nos traemos la imagen
+                    var contenido_popup = `<img src="${imagen_articulo}" alt="Imagen del blog">`;
+                    $('.blog__popup').html(contenido_popup);
+                    console.log(imagen_articulo);
+                    $('.blog__popup, .blog__popup-overlay').show();
+                });
+                // Y que se oculten al pulsar fuera de ellos.
+                $('.blog__popup-overlay').on('click', function () {
+                    $('.blog__popup, .blog__popup-overlay').hide();
+                    console.log("Cierra!!!");
+                });
             });
             
 
